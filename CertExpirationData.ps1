6f318bf5ea4a76adf8cdcc36499b788c083c6fcd
Vipul <45382135+vipullende@users.noreply.github.com>
2022-04-14 22:47:04 +0530

    added taging script

--- CertExpirationData.ps1
+++ CertExpirationData.ps1
@@ -2,7 +2,7 @@
 #   This script is to get the list of certificates which are going to     #
 #    expire in next 30 days                                               #
 ###########################################################################
-#Created by: Vipul Lende                                                  #
+#   Created by: Vipul Lende                                               #
 ###########################################################################
 
 
---
+++ getResourceTags.ps1
@@ -0,0 +1,42 @@
+
+$arrayData = @()
+
+$resources = Get-AzResource  
+
+
+
+foreach($resource in $resources)
+{
+$ResourceName = $resource.name 
+$ResourceTag = $resource.Tags 
+$ResourceType = $resource.Type
+$ResourceGroup = $resource.ResourceGroupName
+$ResourceID = $resource.Id
+
+$Tagdata = ""
+
+foreach($TagDictionary in $ResourceTag)
+{
+     foreach($Key in $TagDictionary.Keys)
+     {
+        $Tagdata += $Key + ":" + $($TagDictionary[$key]) + ","
+     }    
+}
+
+$Report = " " | Select Resource_Name , Tags, Resource_Type, Resource_Group, ReID
+
+$Report.Resource_Name = $ResourceName
+$Report.Tags = $Tagdata.TrimEnd(",")
+$Report.Resource_Type = $ResourceType
+$Report.Resource_Group = $ResourceGroup
+$report.ReID = $ResourceID
+$arraydata += $Report
+
+}
+
+$arrayData #| Export-Csv -NoTypeInformation C:\vipul\Cona\ResourceName_Tags.csv
+
+ 
+
+
+
