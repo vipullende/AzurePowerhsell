d215bc1e24aa284d2c8c6da45267d318e66cf256
Vipul <45382135+vipullende@users.noreply.github.com>
2022-04-14 19:21:32 +0530

    deleted the unwanted file

--- certs_data.ps1
+++
@@ -1,4 +0,0 @@
-$certs = Get-AzResource | where {$_.ResourceType -contains "Microsoft.CertificateRegistration/certificateOrders"}
-$certs.Count
-Get-AzWebApp -ResourceGroupName $certs[0].ResourceGroupName
-Get-AzCertificate -ResourceGroupName rg_nonpsub_m_ame_westus2
