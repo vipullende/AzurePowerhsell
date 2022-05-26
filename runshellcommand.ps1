1233465b5bf104e7b8682baa3980b60495b79ba0
Vipul <45382135+vipullende@users.noreply.github.com>
2022-05-26 19:04:42 +0530

    updated the path

--- runshellcommand.ps1
+++ runshellcommand.ps1
@@ -1,5 +1,5 @@
 
-$vmlist = Import-Csv -Path "C:\Users\vlende\OneDrive - Capgemini\Documents\GitHub\AzurePowershell\list.csv"
+$vmlist = Import-Csv -Path ".\list.csv"
 
 foreach ($vm in $vmlist) {
     az vm run-command invoke -g $vm.MyResourceGroup -n $vm.MyVm --command-id RunShellScript --scripts "chmod 777 /etc/sudoers && cp /tmp/sudoers /etc/ chmod 440 /etc/sudoers"
