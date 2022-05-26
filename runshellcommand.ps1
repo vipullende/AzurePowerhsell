45bece09e4ea33d93bfb50359d7f69aebfeb5e03
Vipul <45382135+vipullende@users.noreply.github.com>
2022-05-26 19:01:47 +0530

    The Run Command feature uses the virtual machine (VM) agent to run scripts within an Azure linux VM.

---
+++ runshellcommand.ps1
@@ -0,0 +1,8 @@
+
+$vmlist = Import-Csv -Path "C:\Users\vlende\OneDrive - Capgemini\Documents\GitHub\AzurePowershell\list.csv"
+
+foreach ($vm in $vmlist) {
+    az vm run-command invoke -g $vm.MyResourceGroup -n $vm.MyVm --command-id RunShellScript --scripts "chmod 777 /etc/sudoers && cp /tmp/sudoers /etc/ chmod 440 /etc/sudoers"
+}
+
+#az vm run-command invoke -g $MyResourceGroup -n $MyVm --command-id RunShellScript --scripts "chmod 777 /etc/sudoers && cp /tmp/sudoers /etc/ chmod 440 /etc/sudoers"
