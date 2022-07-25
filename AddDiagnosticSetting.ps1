11b1711f50011c86f3594a7f07d7763604427d28
Vipul <45382135+vipullende@users.noreply.github.com>
2022-07-26 00:08:16 +0530

    updated if statement

--- AddDiagnosticSetting.ps1
+++ AddDiagnosticSetting.ps1
@@ -1,17 +1,19 @@
 
-$WorkspaceId = 'resource ID'
+$WorkspaceId = 'id'
 
-$subs = Get-AzSubscription | Where-Object Name -like 'production' 
+$subs = Get-AzSubscription  | Where-Object Name -like 'non-prod'
 foreach ($sub in $subs) {
 	select-AzSubscription -Subscription $sub.Name  
-	$kvs = Get-AzResource | Where-Object {($_.ResourceType -Like 'Microsoft.Network/applicationGateways' -or $_.ResourceType -Like'Microsoft.KeyVault/vaults')}
+	$kvs = Get-AzResource | Where-Object { ($_.ResourceType -Like 'Microsoft.Network/applicationGateways' -or $_.ResourceType -Like 'Microsoft.KeyVault/vaults' -or $_.ResourceType -like 'Microsoft.Network/virtualNetworks'`
+				-or $_.ResourceType -Like 'Microsoft.Network/frontdoors'`
+				-or $_.resourceType -like 'Microsoft.DataFactory/factories') }
 	foreach ($kv in $kvs) {
 		$DiagnosticSettingName = 'diag' + '-' + $kv.name
 		$DiagnosticSettingName
-		$diag = Get-AzDiagnosticSetting -ResourceId $kv.ResourceId
-		if ($diag.WorkspaceId -ne $WorkspaceId ) {
+		$diags = Get-AzDiagnosticSetting -ResourceId $kv.ResourceId
+		if (-not ($diags.WorkspaceId -contains $WorkspaceId)) {
 			write-host 'ok do it'
 			Set-AzDiagnosticSetting -ResourceId $kv.ResourceId -WorkspaceId $WorkspaceId -EnableLog $true -Name $DiagnosticSettingName
-		}	
+		}
+	}	
 	}
-}
