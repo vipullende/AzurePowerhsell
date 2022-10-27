65b9df5d487c9fcf680d8678f4a847cb10637db1
Vipul <45382135+vipullende@users.noreply.github.com>
2022-10-27 17:13:30 +0530

    still working on it

---
+++ sqldbTLS.ps1
@@ -0,0 +1,6 @@
+﻿$subs = Get-AzSubscription #| Where-Object Name -like 'production'
+foreach ($sub in $subs) {
+    select-AzSubscription -Subscription $sub.Name 
+    $sqlServer += Get-AzSqlServer
+}
+    $sqlServer | select ServerName, MinimalTlsVersion
