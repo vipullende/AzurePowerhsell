1ff452da62db7d642a08762028c8d233696783b9
Vipul <45382135+vipullende@users.noreply.github.com>
2022-11-09 18:18:36 +0530

    new script for snapshot deletion

---
+++ snapshotdeletion.ps1
@@ -0,0 +1,18 @@
+﻿$subs = Get-AzSubscription | Where-Object Name -notlike Access*
+$snapShotsToBedeleted = @()
+#$snapShotsToBeMailed = @()
+foreach ($Sub in $subs) {
+    Select-AzSubscription  -SubscriptionName $sub.Name 
+    $snapShots = Get-AzSnapshot
+    #$DaysOld = ([datetime] (Get-Date -format yyyy-MM-dd)).AddDays(-45)
+    foreach ($snapShot in $snapShots) {
+        if ([datetime] $snapShot.TimeCreated -lt ([datetime] (Get-Date -format yyyy-MM-dd)).AddDays(-60)) {
+            Write-Host 'Delete' $snapShot.Name  $snapShot.TimeCreated $snapShot.Sku.Name
+            $snapShotsToBedeleted += $snapShot
+            Write-Host 'delete'
+           # Remove-AzSnapshot -ResourceGroupName $snapshot.ResourceGroupName -SnapshotName $snapShot.Name -Force;
+
+        }
+    }
+}
+$snapShotsToBedeleted.CreationData[0]
