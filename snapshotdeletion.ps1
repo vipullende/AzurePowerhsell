044212d26635dcceee98cedca11c778e85c6ee88
Vipul <45382135+vipullende@users.noreply.github.com>
2022-11-16 22:47:59 +0530

    update

--- snapshotdeletion.ps1
+++ snapshotdeletion.ps1
@@ -1,4 +1,8 @@
-﻿$subs = Get-AzSubscription | Where-Object Name -notlike Access*
+﻿#######################################################################
+#  Created by: Vipul Lende                                           #
+######################################################################
+
+$subs = Get-AzSubscription | Where-Object Name -notlike Access*
 $snapShotsToBedeleted = @()
 #$snapShotsToBeMailed = @()
 foreach ($Sub in $subs) {
@@ -10,8 +14,22 @@ foreach ($Sub in $subs) {
             Write-Host 'Delete' $snapShot.Name  $snapShot.TimeCreated $snapShot.Sku.Name
             $snapShotsToBedeleted += $snapShot
             Write-Host 'delete'
-           # Remove-AzSnapshot -ResourceGroupName $snapshot.ResourceGroupName -SnapshotName $snapShot.Name -Force;
+            # Remove-AzSnapshot -ResourceGroupName $snapshot.ResourceGroupName -SnapshotName $snapShot.Name -Force;
 
         }
     }
 }
+# $head = @'
+# <style>
+# BODY{background-color:white;}
+# TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}
+# TH{border-width: 1px;padding: 5px;border-style: solid;border-color: black;foreground-color: black;background-color: LightBlue}
+# TD{border-width: 1px;padding: 5px;border-style: solid;border-color: black;foreground-color: black;background-color: white}
+# .green{background-color:#d5f2d5}
+# .blue{background-color:#e0eaf1}
+# .red{background-color:#ffd7de}
+# </style>
+# '@
+# $snapShotsToBedeleted | select Name, TimeCreated |  ConvertTo-Html -Head $head -Body "<h1>Snapshots which are deleted</h5>`n<h5>Generated on $(Get-Date)</h5>"   | Out-File .\$date"_Data.html"
+
+# $snapShotsToBedeleted.Count
