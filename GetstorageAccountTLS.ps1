3cf402367c86c8071da390ebf6f14375bc369c5e
Vipul <45382135+vipullende@users.noreply.github.com>
2022-11-17 23:27:38 +0530

    update alloed ip

--- GetstorageAccountTLS.ps1
+++ GetstorageAccountTLS.ps1
@@ -1,12 +1,16 @@
 ﻿
 
-$subs = Get-AzSubscription #| Where-Object Name -like 'production'
+$subs = Get-AzSubscription | Where-Object Name -like 'production'
+$stg = @()
 foreach ($sub in $subs) {
     select-AzSubscription -Subscription $sub.Name 
-    $storageAccounts = Get-AzStorageAccount
+    $storageAccounts = Get-AzStorageAccount | Where-Object ResourceGroupName -notlike *pac* 
     foreach ($accountName in $storageAccounts) {
         # Read the MinimumTlsVersion property.MinimumTlsVersion
         $tls = (Get-AzStorageAccount -ResourceGroupName $accountName.ResourceGroupName -Name $accountName.StorageAccountName).MinimumTlsVersion
+        $allowedIP = Get-AzStorageAccountNetworkRuleSet -ResourceGroupName $accountName.ResourceGroupName -AccountName $accountName.StorageAccountName 
+       
+        $allowedIP.IpRules 
         if ($tls -eq 'TLS1_2') {
             write-host 'Hurry' $accountName.StorageAccountName 'is fine'
         }
@@ -15,6 +19,26 @@ foreach ($sub in $subs) {
             Set-AzStorageAccount -ResourceGroupName $accountName.ResourceGroupName `
             -Name $accountName.StorageAccountName `
             -MinimumTlsVersion TLS1_2
+        }
+        if ($allowedIP.DefaultAction -contains 'Deny') {
+
+            $stg += $accountName
+            if ($allowedIP.IpRules.ipaddressorrange -notcontains '10.10.10.10') {
+                Write-Host 'add 10.10.10.10' 
+                Add-AzStorageAccountNetworkRule -ResourceGroupName $accountName.ResourceGroupName`
+                    -AccountName $accountName.StorageAccountName -IPAddressOrRange '161.162.158.164'
+            }
+
+            if ($allowedIP.IpRules.ipaddressorrange -notcontains '10.10.10.11') {
+                Write-Host 'add 10.10.10.11' 
+                Add-AzStorageAccountNetworkRule -ResourceGroupName $accountName.ResourceGroupName`
+                    -AccountName $accountName.StorageAccountName -IPAddressOrRange '10.10.10.11'
+            }
+            if ($allowedIP.IpRules.ipaddressorrange -notcontains '10.10.10.12') {
+                Write-Host 'add 10.10.10.12' 
+                Add-AzStorageAccountNetworkRule -ResourceGroupName $accountName.ResourceGroupName`
+                    -AccountName $accountName.StorageAccountName -IPAddressOrRange '10.10.10.12'
+            }
         }
     }
 }
